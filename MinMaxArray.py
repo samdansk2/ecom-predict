f10db7ca95458752d799fb94e1566857adddb57c
Samdan Shaik <110446718+samdansk2@users.noreply.github.com>
2023-08-25 14:24:08 +0530

    Add files via upload

---
+++ MinMaxArray.py
@@ -0,0 +1,8 @@
+def minmaxi(arr):
+    arr.sort()
+    element={"min":arr[0],"max":arr[-1]}
+    return element
+arr=[15,11,9,6,5,2]
+element= minmaxi(arr)
+print("Minimum is",element["min"])
+print("Maximum is",element["max"])
