c8ab46fd2c40a8177600e7d68eaa9852179201bd
Samdan Shaik <110446718+samdansk2@users.noreply.github.com>
2023-09-06 10:37:34 +0530

    Add files via upload

---
+++ BFS.py
@@ -0,0 +1,27 @@
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited=[]
+queue=[]
+
+def bfs(graph,visited,node):
+    visited.append(node)
+    queue.append(node)
+
+    while(queue):
+        m=queue.pop(0)#removes 0 index eleement
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+print("The Bfs Solution Is:")
+bfs(graph,visited,'5')
+print(visited)
+    
