befbfda32e2ee16ef3f4919b97ae69a36ad09c2b
samdansk2 <samdanshaik8998@gmail.com>
2025-07-28 19:11:11 +0530

    wip

--- count of ele once.py
+++
@@ -1,8 +0,0 @@
-# program to return all the array elements with frequency 1
-
-arr=[1,2,3,2,4,5,4]
-for i in range(len(arr)):
-    temp=arr.count(arr[i])
-    if(temp==1):
-        print(arr[i])
-        
--- frequency of elements in arr.py
+++
@@ -1,9 +0,0 @@
-# program to find frequency of all elements in array 
-
-arr=[2,3,4,3,2]
-arr1=[]
-for i in range(len(arr)):
-    if arr[i] not in arr1:
-        temp=arr.count(arr[i])
-        print(arr[i],"-",temp)
-        arr1.append(arr[i])
